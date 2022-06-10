459201d8923fd846b3992dad5bd3ae8839b34dc0
Josia <scheerle.josia@gmail.com>
2022-06-10 15:34:34 +0200

    Get name of Sunday from ics

--- thumbGen.py
+++ thumbGen.py
@@ -1,18 +1,23 @@
+# -*- coding: iso-8859-1 -*-
 from PIL import Image, ImageDraw, ImageFont
+from icalendar import Calendar
 from datetime import datetime, timedelta
-import json
-
+import os
+import sys
 
 def nextSunday():
     d = datetime.today()
     days_ahead = 6 - d.weekday()
     if days_ahead <= 0: # Target day already happened this week
         days_ahead += 7
-    return (d + timedelta(days_ahead)).strftime('%d.%m.%Y')
+    nextDate = d + timedelta(days_ahead)
+    thumbnailName = getName((nextDate.strftime('%Y-%m-%d')))
+    thumbnailDate = nextDate.strftime('%d.%m.%Y')
+    return thumbnailDate, thumbnailName
 
-
-def createThumbnail(sunday:str, lesson:str):
-    nextDate = nextSunday()
+def modifyThumbnail(lesson):
+    nextDate = nextSunday()[0]
+    sunday = nextSunday()[1]
     image = Image.open("./thumbnail_raw.png")
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype("tahoma.ttf", 90)
@@ -23,9 +28,31 @@ def createThumbnail(sunday:str, lesson:str):
     draw.text((654, 761), lesson, (167, 22, 128), font=font)
 
     #save Thumbnail
-    path = "./Thumbnail " + nextDate + ".png"
+    path = './Thumbnail ' + nextDate + '.png'
     image.save(path)
+    print('Thumbnail was successfully generated')
+    os.startfile(os.getcwd())
 
-with open('./thumbnail.json') as f:
-    data = json.load(f)
-createThumbnail(data['name'], data['lesson'])
+def createCurrentThumbnail():
+    lesson = input('Enter the lesson for this Sunday: ')
+    modifyThumbnail(lesson.upper())
+
+def getName(date):
+    g = open('kirchenjahr-evangelisch.ics','rb')
+    gcal = Calendar.from_ical(g.read())
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            if date == str(component.decoded('dtstart')):
+                name = component.get('summary').upper()
+                g.close()
+                return name
+    g.close()
+    print('No matching result was found! Check if the Calendar is up to date.')
+    input('Press any key to close the application...')
+    sys.exit()
+
+createCurrentThumbnail()
+
+# for a future version make the filename of the calendar dynamic
+#f = []
+#os.walk(dirpath, dirnames)
